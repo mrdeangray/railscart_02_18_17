36b14a6c8239b8769b0ff8393e6f3c866c76d2f3
Dean <mrdeangray@gmail.com>
2017-03-04 14:50:26 +0000

    migrated product db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226154548) do
+ActiveRecord::Schema.define(version: 20170304142839) do
 
   create_table "objectives", force: true do |t|
     t.string   "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20170226154548) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "completed_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "projects", force: true do |t|
